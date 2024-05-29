77db47e471f7c01002b42d8b05fdd45104724dde
Charles Klein <charlesklein@berkeley.edu>
2024-05-28 22:24:49 -0700

    webcam up and running

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/live-feed')
+def live_feed():
+    return render_template('live-feed.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
